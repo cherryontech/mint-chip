6a2d6e0c12917925478651e1eb588875db2d7478
Mag <166319982+magali-la@users.noreply.github.com>
2025-10-19 02:23:03 -0400

    feat(onboarding): add centered quiz card container with shadow

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,6 +1,12 @@
+import Button from '../components/Button';
+
 export default function Onboarding() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-nyanza via-celeste via-50% to-white to-50%">
+    <div className="flex justify-center items-center h-screen bg-gradient-to-b from-nyanza via-celeste via-50% to-white to-50%">
+      {/* quiz card */}
+      <div className="flex bg-white rounded-[10px] h-[80vh] w-[60vw] shadow-[0_4px_15px_8px_rgba(30,30,30,0.10)]">
+        
+      </div>
     </div>
   );
 }
