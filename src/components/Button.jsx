c65836c7bfb560bba2fd4f12c9dc5cb99045d1a6
CMolinaBetancourt <cathemolinab@gmail.com>
2025-10-23 15:24:27 -0700

    add react-router-dom link to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import { tv } from 'tailwind-variants/lite';
+import { Link } from 'react-router-dom';
 
 const buttonVariants = tv({
     // base styles for all buttons
@@ -32,6 +33,7 @@ const buttonVariants = tv({
         }
     ]
 });
+
 export default function Button({ size, color, label, className, to }) {
   const classes = `${buttonVariants({ size, color })} ${className || ''}`;
 
