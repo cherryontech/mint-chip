8596f5ba2b5c99f23674de90329976f9ac254026
CMolinaBetancourt <cathemolinab@gmail.com>
2025-10-23 15:05:09 -0700

    remove code from button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,39 +1,37 @@
 import { tv } from 'tailwind-variants/lite';
-import { Link } from 'react-router-dom';
 
 const buttonVariants = tv({
-  // base styles for all buttons
-  base: 'flex items-center justify-center drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]',
-  // all button variants
-  variants: {
-    size: {
-      sm: 'h-14 w-[220px] text-2xl text-md font-semibold rounded-lg',
-      md: 'h-16 w-82 text-2xl font-semibold rounded-lg',
-      lg: 'h-11 w-[501px] text-lg font-medium rounded-md',
-      circ: 'h-11 w-11 text-2xl rounded-full',
+    // base styles for all buttons
+    base: 'flex items-center justify-center drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]',
+    // all button variants
+    variants: {
+        size: {
+            sm: 'h-14 w-[220px] text-2xl text-md font-semibold rounded-lg',
+            md: 'h-16 w-82 text-2xl font-semibold rounded-lg',
+            lg: 'h-11 w-[501px] text-lg font-medium rounded-md',
+            circ: 'h-11 w-11 text-2xl rounded-full'
+        },
+        color: {
+            primary: 'bg-[#1E1E1E] text-white border-1 border-[#1E1E1E]',
+            secondary: 'bg-white text-[#1E1E1E] border-1 border-[#1E1E1E]',
+            gradient: 'bg-gradient-to-b from-[#15F30D] to-[#0561A7] text-white'
+        }
     },
-    color: {
-      primary: 'bg-[#1E1E1E] text-white border-1 border-[#1E1E1E]',
-      secondary: 'bg-white text-[#1E1E1E] border-1 border-[#1E1E1E]',
-      gradient: 'bg-gradient-to-b from-[#15F30D] to-[#0561A7] text-white',
+    // default button styles if no specified props
+    defaultVariants: {
+        size: 'sm',
+        color: 'primary'
     },
-  },
-  // default button styles if no specified props
-  defaultVariants: {
-    size: 'sm',
-    color: 'primary',
-  },
-  // conditional style cases for specific prop combinations
-  compoundVariants: [
-    // remove drop shadow and lower font weight for onboarding quiz button
-    {
-      color: 'secondary',
-      size: 'md',
-      className: 'drop-shadow-none !font-normal',
-    },
-  ],
+    // conditional style cases for specific prop combinations
+    compoundVariants: [
+        // remove drop shadow and lower font weight for onboarding quiz button
+        {
+            color: 'secondary',
+            size: 'md',
+            className: 'drop-shadow-none !font-normal'
+        }
+    ]
 });
-
 export default function Button({ size, color, label, className, to }) {
   const classes = `${buttonVariants({ size, color })} ${className || ''}`;
 
