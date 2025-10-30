4c3f6e1e896b6f5ddf06c9e6df20258d8beeb740
Mag <166319982+magali-la@users.noreply.github.com>
2025-10-29 21:50:45 -0400

    feat(onboarding): added responsive full page gradient background on mobile

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -2,7 +2,7 @@ import Button from '../components/Button';
 
 export default function Onboarding() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-b from-nyanza via-celeste via-50% to-white to-50%">
+    <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-b from-nyanza to-celeste sm:via-celeste sm:via-50% sm:to-white sm:to-50%">
       {/* quiz card */}
       <div className="flex flex-col justify-center items-center gap-10 bg-white rounded-[10px] h-[80vh] w-[60vw] shadow-[0_4px_15px_8px_rgba(30,30,30,0.10)]">
         {/* title and subtitle */}
