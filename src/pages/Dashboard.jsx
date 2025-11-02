27f51f9e1e43aab5f5e2060644b245040178299b
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-02 11:54:28 -0500

    feat(dashboard): added title and button container and included heading font

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
-function Dashboard() {
+import Button from "../components/Button";
+
+export default function Dashboard() {
   return (
-    <div className="min-h-screen bg-emerald-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-lime-500">Dashboard Page! 🎉</h1>
+      <div className="bg-white mx-[144px]">
+      {/* title and button */}
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl font-playfair font-semibold text-eerie place-items-start mt-4">Welcome Back!</h1>
+          <div className="w-full place-items-center">
+            <Button 
+              size='md'
+              color='primary'
+              label='Start a Task' 
+              className="place-items-center"
+            />
+          </div>
+        </div>
     </div>
   );
 }
-
-export default Dashboard;
