8e354c36de0d28c2c82918ecf395acadaa411a54
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-06 00:19:07 -0500

    style(onboarding): Add playfair display and poppins to onboarding page

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -30,10 +30,10 @@ export default function Onboarding() {
       <div className="flex flex-col justify-center items-center gap-6 sm:gap-8 md:gap-10 bg-white rounded-[10px] h-[90vh] sm:h-[80vh] w-full sm:w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[65vw] 2xl:w-[60vw] shadow-[0_4px_15px_8px_rgba(30,30,30,0.10)]">
         {/* title and subtitle */}
         <div className="flex flex-col gap-2 md:gap-3 text-center">
-          <h1 className="text-base sm:text-xl md:text-2xl lg:text-3xl font-medium">
+          <h1 className="font-playfair text-base sm:text-xl md:text-2xl lg:text-3xl font-medium">
             What are some of your concerns today?
           </h1>
-          <p className="text-base sm:text-lg lg:text-2xl font-normal">
+          <p className="font-poppins text-base sm:text-lg lg:text-2xl font-normal">
             Your selections help us customize your dashboard
           </p>
         </div>
